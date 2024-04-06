422ef9363fcdb59b4781923f72eb1ca8173a307f
toddlf705 <toddlf705@gmail.com>
2024-04-06 13:34:46 -0400

    search and filter icons added to nav bar

--- wimf_app/src/components/Nav.jsx
+++ wimf_app/src/components/Nav.jsx
@@ -1,15 +1,27 @@
 import { useNavigate } from 'react-router-dom'
+import search_icon from '../assets/search_icon.png'
+import filter_icon from '../assets/filter_icon.png'
 
 const Nav = () => {
     const navigate = useNavigate()
 
     return (
         <div className='nav-container'>
-            <div className='nav'>
-                <button onClick={()=>navigate('/grocery_list')}>Grocery List</button>
+            <div className='nav-menu'>
+                <div className='nav'>
+                    <button onClick={()=>navigate('/grocery_list')}>Grocery List</button>
+                </div>
+                <div className='nav'>
+                    <button onClick={()=>navigate('/my_fridge')}>My Fridge</button>
+                </div>
             </div>
-            <div className='nav'>
-                <button onClick={()=>navigate('/my_fridge')}>My Fridge</button>
+            <div className='nav-icons'>
+                <div className='search'>
+                    <button><img src={search_icon} width='30px'/></button>
+                </div>
+                <div className='filter'>
+                    <button><img src={filter_icon} width='30px'/></button>
+                </div>
             </div>
         </div>
     )
